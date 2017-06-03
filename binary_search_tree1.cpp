65f9dd513376174cd23d6a0728fcca092ba972ed
asemchenko <andriysemchenko@gmail.com>
2017-06-03 11:09:49 +0300

    added removing element

--- binary_search_tree1.cpp
+++ binary_search_tree1.cpp
@@ -49,79 +49,73 @@ public:
 			prev_node->key < node->key ? prev_node->right_child : prev_node->left_child = node;
 		}
 	}
-Node* get_root_ptr()
-{
-	return root;
-}
-void print_sorted(Node* start)
-{
-	if (start != NULL)
+	Node* get_root_ptr()
 	{
-		print_sorted(start->left_child);
-		std::cout << start->key << std::endl;
-		print_sorted(start->right_child);
+		return root;
 	}
-	return;
-}
-Node* get_min_value()
-{
-	Node* n = root;
-	if (n)
+	void print_sorted(Node* start)
 	{
-		while (n->left_child != NULL)
+		if (start != NULL)
 		{
-			n = n->left_child;
+			print_sorted(start->left_child);
+			std::cout << start->key << std::endl;
+			print_sorted(start->right_child);
 		}
-		return n;
+		return;
 	}
-	return NULL;
-}
-Node* get_min_value(Node* n)
-{
-	if (n)
+	Node* get_min_value()
 	{
-		while (n->left_child != NULL)
+		Node* n = root;
+		if (n)
 		{
-			n = n->left_child;
+			while (n->left_child != NULL)
+			{
+				n = n->left_child;
+			}
+			return n;
 		}
-		return n;
-	}
-	return NULL;
-}
-Node* get_max_value()
-{
-	Node* n = root;
-	if (n)
-	{
-		while (n->right_child)
-		{
-			n = n->right_child;
-		}
-		return n;
-	}
-	return NULL;
-}
-Node* get_max_value(Node* n)
-{
-	if (n)
-	{
-		while (n->right_child)
-		{
-			n = n->right_child;
-		}
-		return n;
-	}
-	return NULL;
-}
-Node* search(keyT key)
-{
-	Node* n = root;
-	if (!n)
-	{
 		return NULL;
 	}
-	else
+	Node* get_min_value(Node* n)
 	{
+		if (n)
+		{
+			while (n->left_child != NULL)
+			{
+				n = n->left_child;
+			}
+			return n;
+		}
+		return NULL;
+	}
+	Node* get_max_value()
+	{
+		Node* n = root;
+		if (n)
+		{
+			while (n->right_child)
+			{
+				n = n->right_child;
+			}
+			return n;
+		}
+		return NULL;
+	}
+	Node* get_max_value(Node* n)
+	{
+		if (n)
+		{
+			while (n->right_child)
+			{
+				n = n->right_child;
+			}
+			return n;
+		}
+		return NULL;
+	}
+	Node* search(keyT key)
+	{
+		Node* n = root;
 		while (n)
 		{
 			if (n->key == key)
@@ -130,106 +124,199 @@ Node* search(keyT key)
 			}
 			n = n->key < key ? n->right_child : n->left_child;
 		}
+		return NULL;
 	}
-}
-Node* get_preccessor(keyT key)
-{
-	Node* input_node = search(key);
-	if (input_node != NULL)
+	Node* get_preccessor(keyT key)
 	{
-		if (input_node->left_child)
+		Node* input_node = search(key);
+		if (input_node != NULL)
 		{
-			return get_max_value(input_node->left_child);
+			if (input_node->left_child)
+			{
+				return get_max_value(input_node->left_child);
+			}
+			else
+			{
+				Node* prev_node = input_node->parent;
+				while (prev_node && (prev_node->right_child == input_node))
+				{
+					input_node = prev_node;
+					prev_node = prev_node->parent;
+				}
+				return prev_node;
+			}
 		}
 		else
 		{
-			Node* prev_node = input_node->parent;
-			while (prev_node && (prev_node->right_child == input_node) )
-			{
-				input_node = prev_node;
-				prev_node = prev_node->parent;
-			}
-			return prev_node;
+			return NULL;
 		}
 	}
-	else
+	Node* get_preccessor(Node* input_node)
 	{
-		return NULL;
-	}
-}
-Node* get_preccessor(Node* input_node)
-{
-	if (input_node != NULL)
-	{
-		if (input_node->left_child)
+		if (input_node != NULL)
 		{
-			return get_max_value(input_node->left_child);
+			if (input_node->left_child)
+			{
+				return get_max_value(input_node->left_child);
+			}
+			else
+			{
+				Node* prev_node = input_node->parent;
+				while (prev_node && (prev_node->left_child == input_node))
+				{
+					input_node = prev_node;
+					prev_node = prev_node->parent;
+				}
+				return prev_node;
+			}
 		}
 		else
 		{
-			Node* prev_node = input_node->parent;
-			while (prev_node && (prev_node->left_child == input_node))
-			{
-				input_node = prev_node;
-				prev_node = prev_node->parent;
-			}
-			return prev_node;
+			return NULL;
 		}
 	}
-	else
+	Node* get_successor(keyT key)
 	{
-		return NULL;
-	}
-}
-Node* get_successor(keyT key)
-{
-	Node* input_node = search(key);
-	if (input_node)
-	{
-		if (input_node->right_child)
+		Node* input_node = search(key);
+		if (input_node)
 		{
-			return get_min_value(input_node->right_child);
+			if (input_node->right_child)
+			{
+				return get_min_value(input_node->right_child);
+			}
+			else
+			{
+				Node* p = input_node->parent;
+				while ((p) && (p->right_child == input_node))
+				{
+					input_node = p;
+					p = p->parent;
+				}
+				return p;
+			}
 		}
 		else
 		{
-			Node* p = input_node->parent;
-			while ((p) && (p->right_child == input_node))
-			{
-				input_node = p;
-				p = p->parent;
-			}
-			return p;
+			return NULL;
 		}
 	}
-	else
+	Node* get_successor(Node* input_node)
 	{
-		return NULL;
-	}
-}
-Node* get_successor(Node* input_node)
-{
-	if (input_node)
-	{
-		if (input_node->right_child)
+		if (input_node)
 		{
-			return get_min_value(input_node->right_child);
+			if (input_node->right_child)
+			{
+				return get_min_value(input_node->right_child);
+			}
+			else
+			{
+				Node* p = input_node->parent;
+				while ((p) && (p->right_child == input_node))
+				{
+					input_node = p;
+					p = p->parent;
+				}
+				return p;
+			}
 		}
 		else
 		{
-			Node* p = input_node->parent;
-			while ((p) && (p->right_child == input_node))
-			{
-				input_node = p;
-				p = p->parent;
-			}
-			return p;
+			return NULL;
 		}
 	}
-	else
+	bool remove(keyT key)
 	{
-		return NULL;
+		Node* removing_node = search(key);
+		if (removing_node)
+		{
+			Node* fact_rm_node; // node, which we will physically remove
+			if ((removing_node->right_child) && (removing_node->left_child))
+			{
+				fact_rm_node = get_successor(removing_node);
+				removing_node->key = fact_rm_node->key;
+			}
+			else
+			{
+				fact_rm_node = removing_node;
+			}
+			// fact_rm_node always has'nt one child
+			Node* child;
+			if (fact_rm_node->left_child)
+			{
+				child = fact_rm_node->left_child;
+			}
+			else
+			{
+				child = fact_rm_node->right_child;
+			}
+			if (child)
+			{
+				child->parent = fact_rm_node->parent;
+				// if we deleting root
+				if (fact_rm_node->parent == NULL)
+				{
+					root = child;
+				}
+				fact_rm_node->parent->right_child == fact_rm_node ? fact_rm_node->parent->right_child : fact_rm_node->parent->left_child = child;
+			}
+			else
+			{
+				fact_rm_node->parent->right_child == fact_rm_node ? fact_rm_node->parent->right_child : fact_rm_node->parent->left_child = NULL;
+			}
+			delete fact_rm_node;
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
-}
+	bool remove(Node* removing_node)
+	{
+		if (removing_node)
+		{
+			Node* fact_rm_node; // node, which we will physically remove
+			if ((removing_node->right_child) && (removing_node->left_child))
+			{
+				fact_rm_node = get_successor(removing_node);
+				removing_node->key = fact_rm_node->key;
+			}
+			else
+			{
+				fact_rm_node = removing_node;
+			}
+			// fact_rm_node always has'nt one child
+			Node* child;
+			if (fact_rm_node->left_child)
+			{
+				child = fact_rm_node->left_child;
+			}
+			else
+			{
+				child = fact_rm_node->right_child;
+			}
+			if (child)
+			{
+				child->parent = fact_rm_node->parent;
+				// if we deleting root
+				if (fact_rm_node->parent == NULL)
+				{
+					root = child;
+				}
+				fact_rm_node->parent->right_child == fact_rm_node ? fact_rm_node->right_child : fact_rm_node->left_child = child;
+			}
+			else
+			{
+				fact_rm_node->parent->right_child == fact_rm_node ? fact_rm_node->right_child : fact_rm_node->left_child = NULL;
+			}
+			delete fact_rm_node;
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
 private:
 	Node* root = NULL;
 	void erase_memory(Node* vertex_start)
@@ -250,7 +337,9 @@ int main()
 	{
 		tree.add_element(i);
 	}
-	std::cout<<tree.get_successor(tree.search(12))->key<<std::endl;
+	std::cout << tree.get_successor(tree.search(12))->key << std::endl;
+	tree.remove(13);
+	std::cout << tree.get_successor(tree.search(12))->key << std::endl;
 	system("pause");
 	return 0;
 }
